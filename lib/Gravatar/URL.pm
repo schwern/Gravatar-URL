d384665123685b51bd86a5fc2ad7cff698bd2ffe
Michael G. Schwern <schwern@pobox.com>
2012-03-27 13:00:54 +0100

    Version 1.06

--- lib/Gravatar/URL.pm
+++ lib/Gravatar/URL.pm
@@ -7,7 +7,7 @@ use URI::Escape qw(uri_escape);
 use Digest::MD5 qw(md5_hex);
 use Carp;
 
-our $VERSION = '1.05';
+our $VERSION = '1.06';
 
 use parent 'Exporter';
 our @EXPORT = qw(
--- lib/Libravatar/URL.pm
+++ lib/Libravatar/URL.pm
@@ -7,7 +7,7 @@ use Gravatar::URL qw(gravatar_url);
 use Digest::SHA qw(sha256_hex);
 use Carp;
 
-our $VERSION = '1.05';
+our $VERSION = '1.06';
 
 use parent 'Exporter';
 our @EXPORT = qw(
